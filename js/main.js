1dd1e9a15697dda1dbb87efe9f6df1b516144e92
julielo5 <julielo.inbox@gmail.com>
2017-01-27 15:55:00 -0800

    Added function to filter and display images from API.

--- js/main.js
+++ js/main.js
@@ -6,29 +6,38 @@ $(function() {
   $('select').change('click', function () {
       // console.log(this.value);
 
-      var url = 'https://api.nytimes.com/svc/topstories/v2/';
-      url += this.value;
-      url += '.json';
-      url += '?' + $.param({'api-key': 'd4c685c39c7d41bba145a35a29acd516'});
+    var url = 'https://api.nytimes.com/svc/topstories/v2/';
+    url += this.value;
+    url += '.json';
+    url += '?' + $.param({'api-key': 'd4c685c39c7d41bba145a35a29acd516'});
 
-      $.ajax({
-        url: url,
-        method: 'GET',
-      }).done(function(data) {
-        var resultData = ''
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(data) {
+      var resultData = ''
+      var $dataSet = data.results.filter(function (item) {
+      return item.multimedia.length;
+      }).splice(0, 12);
 
-        $.each(data.results, function(key, value) {
-        // console.log(value.title);
-         resultData += '<li>';
-         resultData += value.title;
-         resultData += '</li>';
+      $.each($dataSet, function(key, value) {
+        resultData += '<li>';
+        resultData += '<a href=';
+        resultData += value.url;
+        resultData += '>';
+        resultData += '<img src=';
+        resultData += value.multimedia[4].url;
+        resultData += ' alt='
+        resultData += value.title;
+        resultData += '>';
+        resultData += '</a>';
+        resultData += '</li>';
+      });
 
-       });
+      $('.apiList').append(resultData);
 
-      $apiList.append(resultData);
-
-    }).fail(function(err) {
-  throw err;
+    }).fail(function() {
+      console.log('This did not work')
     });
   })
 });
